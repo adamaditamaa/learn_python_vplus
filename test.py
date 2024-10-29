308b17675b0885a635ed0e2241dce8164ee3c66a
adamaditama <adamaditama123@gmail.com>
2024-10-29 17:39:26 +0700

    new learning python

---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+
+
+
+current_script_path = os.path.abspath(__file__)
+main_folder = os.path.dirname(current_script_path)
+folder_config = 'folder_config'
+
+
+
+
+print(main_folder)
